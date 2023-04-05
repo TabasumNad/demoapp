b9a0d198d8b4f4ed5666072504004c4dfa3c92d1
TabasumNad <tmnadaf17@gmail.com>
2023-04-05 23:39:50 +0530

    FSD mobile api demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { useEffect } from 'react';
+
+const API="https://node2-tabasumnad.vercel.app";
+// const API="http://localhost:4000";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Phone/> */}
+      <PhoneList/>
+      
+      
     </div>
   );
 }
+
+function PhoneList(){
+  const [mobileList, setMobilrList]=useState([]);
+
+  useEffect(()=>{
+    fetch(`${API}/mobile`)
+    .then((res)=>res.json())
+    .then((data)=>setMobilrList(data));
+  },[]);
+
+  
+
+
+  return(
+    <div className="phone-list-container">
+      
+     {
+     mobileList.map((mbl,index)=>(
+     <Phone mobile={mbl} key={mbl._id} />
+    ))} 
+    </div>
+  )
+
+}
+
+
+// componenet declaration || { mobile }-->object destructuring (ES6)
+function Phone({mobile}){
+  // const mobile={
+  //   "model": "OnePlus 9 5G",
+  //   "img": "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
+  //   "company": "Oneplus"
+  // };
+
+  return (
+    <div className="phone-container">
+      <img src={mobile.img} alt={mobile.img} className="phone-picture"/>
+<h2 className="phone-name">{mobile.model}</h2>
+<h5 className="phone-company">{mobile.company}</h5>
+    </div>
+  );
+}
+
 
 export default App;
